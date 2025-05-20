93fd9a5a3287e93bdf340941f10c7e02efed1521
ayyappa54 <ayyappa.c@eficens.com>
2025-05-20 16:07:09 +0530

    Added CRUD operations: POST, PUT, and DELETE support for books

--- books.py
+++ books.py
@@ -17,9 +17,9 @@ async def read_books():
     return BOOKS    
 
 
-@app.get("/books/{dynamic_param}")
-async def read_all_books(dynamic_param: str):
-    return {'dynamic_param': dynamic_param}
+# @app.get("/books/{dynamic_param}")
+# async def read_all_books(dynamic_param: str):
+#     return {'dynamic_param': dynamic_param}
 
 @app.get("/books/{book_title}")
 async def read_all_books(book_title: str):
@@ -27,8 +27,6 @@ async def read_all_books(book_title: str):
             if book.get('title').casefold() == book_title.casefold():
                 return book
             
-
-
 
 @app.get("/books/")
 async def read_category_books(category: str):
@@ -39,10 +37,13 @@ async def read_category_books(category: str):
      return books_to_return
 
 
-
-
-
-
+@app.get("/books/byauthor/")
+async def read_books_by_author_path (author: str):
+    books_to_return = [ ]
+    for book in BOOKS:
+     if book.get('author').casefold () == author.casefold():
+       books_to_return.append(book)
+    return books_to_return
 
 @app.get("/books/{book_author}/")
 async def read_auther_category__by_query(book_author: str, category: str):
@@ -64,3 +65,11 @@ async def update_book (updated_book=Body()):
     for i in range(len(BOOKS)) :
      if BOOKS [i].get('title').casefold () == updated_book.get('title').casefold():
         BOOKS [i] = updated_book
+
+
+@app.delete("/boooks/delete_book/")
+async def delete_book(book_title:  str):
+    for i in range(len(BOOKS)):
+        if  BOOKS[i].get('title').casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            break
